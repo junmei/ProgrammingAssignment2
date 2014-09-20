bd326812809076125b16fbaac9ca8454dc82ab10
junmei <junmei.duan@gmail.com>
2014-09-20 14:45:10 +0200

    Calculate the inverse of a matrix
    Finished implementing the two functions in the R file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file provide two functions for calculating the inverse of a matrix and cache it.
+##
+## Usage example: 
+##     m <- makeCacheMatrix(matrix(c(1, 0.25, 0.25, 1), 2, 2))
+##     cacheSolve(m)
 
-## Write a short comment describing this function
 
+## This function caches the inverse of matrix 'x'
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  getsolve <- function() s
+  setsolve <- function(solve) s<<-solve
+  list(set=set, get=get, getsolve=getsolve, setsolve= setsolve)
 }
 
 
-## Write a short comment describing this function
-
+## This function calculates the inverse of matrix 'x'.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+
+  ## If there is a cached value, return it directly.
+  if (!is.null(s)) {
+    message("Get solve from cache")
+    return(s)
+  }
+  
+  # If there is no cached value, calcute the inverse and save it to cache.
+  data <- x$get()
+  s<-solve(data, ...)
+  x$setsolve(s)
+  
+  ## Return a matrix that is the inverse of 'x'.
+  s
 }
